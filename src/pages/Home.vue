c57033c14ff5c6ad3352941a39175b5a8f4b83c6
Jamelame45 <114234222+Jamelame45@users.noreply.github.com>
2022-10-08 15:50:36 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <Navbar />
+        <section class="home">
+            <div class="image">
+                <img src="images/profile.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>hi, i am {{name}}</h3>
+                <span>{{motto}}</span>
+                <p>{{content}}</p>
+                <router-link to="/about" class="btn"> about me <i class="fas fa-user"></i> </router-link>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+export default {
+    components: { Navbar },
+    data() {
+        return {
+            name: 'Natthaphol lapchai',
+            motto: 'Live for Tomorrow',
+            content: ' I am a student of the Faculty of Engineering. Multimedia and Entertainment 2nd year with skills in graphics, drawing, photography and music.',
+        }
+}
+}
+</script>
+
+<style>
+
+</style>
